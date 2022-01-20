c590b26ff8605382443a70e2e10104214968e3bd
nosknut <nosknut@gmail.com>
2022-01-20 13:18:06 +0100

    Fixes import issue by adding a getBoard() function

--- board_mock/board_mock.ino
+++ board_mock/board_mock.ino
@@ -1,39 +1,24 @@
-#pragma once
-
-#include "Arduino.h"
-#include "mockable_board/interfaces/BoardIo.h"
-#include "mockable_board/interfaces/SerialController.h"
-
-#ifndef debug
-#define debug 0
-#endif
-
-#if (debug)
-#include "mockable_board/implementations/virtual/VirtualBoardIo.h"
-#include "mockable_board/implementations/virtual/VirtualSerialController.h"
-BoardIo board = VirtualBoardIo(VirtualSerialController());
-#else
-#include "mockable_board/implementations/physical/PhysicalBoardIo.h"
-#include "mockable_board/implementations/physical/PhysicalSerialController.h"
-BoardIo board = PhysicalBoardIo(PhysicalSerialController());
-#endif // if(debug)
+#include <Arduino.h>
+#include <MockableBoard.h>
 
 #define debug 0
+BoardIo board = getBoard();
 
 void setup()
 {
+    board.digitalRead(0);
     board.serial.begin(9600);
     board.pinMode(13, OUTPUT);
     board.pinMode(12, INPUT);
 
-    Serial.println("sadghjk");
+    Serial.println("Hello World!");
 
     digitalWrite(A0, 255);
 }
 
 void loop()
 {
-    board.serial.println("1");
+    board.serial.println("looping ...");
     board.digitalRead(12);
     board.delay(1000);
     board.digitalWrite(13, HIGH);
---
+++ board_mock/mockable_board/MockableBoard.cpp
@@ -0,0 +1,21 @@
+#include "interfaces/BoardIo.h"
+#include "interfaces/SerialController.h"
+
+#ifndef debug
+#define debug 0
+#endif
+
+#if (debug)
+#include "implementations/virtual/VirtualBoardIo.h"
+#include "implementations/virtual/VirtualSerialController.h"
+BoardIo board = VirtualBoardIo(VirtualSerialController());
+#else
+#include "implementations/physical/PhysicalBoardIo.h"
+#include "implementations/physical/PhysicalSerialController.h"
+BoardIo board = PhysicalSerialController(PhysicalSerialController());
+#endif // if(debug)
+
+BoardIo getBoard()
+{
+    return board;
+}
